83a5e2b4136fb15ff2e86d492cc4e81b8e19d87a
Dylan Murray <dymurray@redhat.com>
2017-03-03 10:49:19 -0500

    Updated hosts file and setup script to use origin deployment

--- setup.sh
+++ setup.sh
@@ -13,20 +13,7 @@ fi
 yum -y install epel-release
 yum -y install deltarpm wget git net-tools bind-utils bridge-utils gcc docker vim ansible
 
-yum -y install subscription-manager
 yum -y update
-
-subscription-manager register --username="qa@redhat.com" --password="CZvGMy7TwJpVKW9t"
-
-subscription-manager attach --pool=8a85f9823e3d5e43013e3ddd4e2a0977
-
-subscription-manager repos --disable *
-
-subscription-manager repos \
-    --enable="rhel-7-server-rpms" \
-    --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-optional-rpms" \
-    --enable="rhel-7-server-ose-3.4-rpms"
 
 groupadd docker
 gpasswd -a vagrant docker
@@ -36,6 +23,7 @@ systemctl enable docker; systemctl start docker
 cp /vagrant/id_rsa* /home/vagrant/.ssh/
 cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 cp -R /home/vagrant/.ssh /root/.ssh
+chmod 600 /root/.ssh/*
 
 wget https://github.com/openshift/origin/releases/download/v1.5.0-alpha.3/openshift-origin-client-tools-v1.5.0-alpha.3-cf7e336-linux-64bit.tar.gz -O /tmp/oc.tar.gz
 
@@ -52,4 +40,7 @@ if [ $1 == "master" ]; then
   git clone https://www.github.com/openshift/openshift-ansible.git; cd openshift-ansible
   cp /vagrant/hosts /etc/ansible/hosts
   ansible-playbook playbooks/byo/config.yml
+#  wget https://github.com/openshift/origin/releases/download/v1.5.0-alpha.3/openshift-origin-server-v1.5.0-alpha.3-cf7e336-linux-64bit.tar.gz -O /tmp/origin.tar.gz
+#  tar -xzf /tmp/origin.tar.gz -C /tmp
+#  mv /tmp/openshift-origin-server-v1.5.0-alpha.3-cf7e336-linux-64bit/openshift /usr/local/bin
 fi
